c6d6917e5dce3aebb721378ab08bf2dce11e7444
Pedro Henrique Bianchi <pedro.h.bianchi@gmail.com>
2021-05-08 22:42:18 -0300

    Matrix abstraction improved TSP_a280 solved with more than one solution, on the same execution Improved the way files were being accessed

--- com/tsp/tsp_a280.py
+++ com/tsp/tsp_a280.py
@@ -1,33 +1,65 @@
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
-from com.utils.matrix import Matrix
+from com.utils.intMatrix import IntMatrix
+from os import getcwd
+from os.path import dirname
 
 
 def create_data_model():
-    distance_matrix = Matrix(rows_size=280, columns_size=280)
-    distance_matrix.construct_matrix_from_euclid_int_dist_2d_file(file_path=r"C:\Users\Primo\Desktop\TSP_a280.txt")
+    distance_matrix = IntMatrix(rows_size=280, columns_size=280)
+    local_file_path = r"{}\tsp\files\TSP_a280.txt".format(dirname(getcwd()))
+    distance_matrix.construct_matrix_from_euclid_int_dist_2d_file(file_path=local_file_path)
 
     data = {'distance_matrix': distance_matrix.matrix, 'num_vehicles': 1, 'depot': 0}
     return data
 
 
-def print_solution(manager, routing, solution):
+def print_solution(manager, routing, solution, strategy, time_limit=None):
     """Prints solution on console."""
-    print('Objective: {} Unit of Measure'.format(solution.ObjectiveValue()))
+    if time_limit is None:
+        print(f'Solution achieved by {strategy} strategy')
+    else:
+        print(f'Solution achieved by {strategy} strategy with time limit on {time_limit} seconds')
+    print(f'Objective: {format(solution.ObjectiveValue())} Unit of Measure')
     index = routing.Start(0)
     plan_output = 'Route for vehicle 1:\n'
     route_distance = 0
     while not routing.IsEnd(index):
-        plan_output += ' {} ->'.format(manager.IndexToNode(index) + 1)
+        plan_output += f' {format(manager.IndexToNode(index) + 1)} ->'
         previous_index = index
         index = solution.Value(routing.NextVar(index))
         route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += ' {}\n'.format(manager.IndexToNode(index) + 1)
+    plan_output += f' {format(manager.IndexToNode(index) + 1)}\n'
     print(plan_output)
-    plan_output += 'Route distance: {}miles\n'.format(route_distance)
 
 
-def main():
+def print_opt_solution(opt_file_path, distance_matrix):
+    opt_file = open(file=opt_file_path, mode='r')
+    plan_output = 'Route on opt file:\n'
+    route_distance = 0
+    index = 0
+    previous_index = 0
+
+    for line in opt_file:
+        index = int(line.strip()) - 1
+        plan_output += f'{index + 1} -> '
+        route_distance += distance_matrix[index][previous_index]
+        previous_index = index
+
+    plan_output += '1'
+    route_distance += distance_matrix[index][0]
+
+    print(f'Objective: {route_distance} Unit of Measure')
+    print(plan_output)
+
+
+def print_solution_and_opt_solution(manager, routing, solution, search_parameters, opt_file_path, distance_matrix):
+    print_solution(manager, routing, solution, search_parameters)
+    print('\n')
+    print_opt_solution(opt_file_path, distance_matrix)
+
+
+def main(time_limit, log_search):
     """Entry point of the program."""
     # Instantiate the data problem.
     data = create_data_model()
@@ -35,6 +67,7 @@ def main():
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
 
+    ''' Solving with first solution heuristic - PATH_CHEAPEST_ARC '''
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
@@ -50,19 +83,58 @@ def main():
     # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Setting first solution heuristic.
+    # Define strategy to solve the problem
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.GLOBAL_CHEAPEST_ARC)#PATH_CHEAPEST_ARC)
+    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
 
-    # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
 
-    # Print solution on console.
     if solution:
-        print_solution(manager, routing, solution)
+        print_solution(manager, routing, solution, strategy="PATH_CHEAPEST_ARC")
+
+    ''' Solving with first solution heuristic - GLOBAL_CHEAPEST_ARC '''
+    # Resetting Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc after reset.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Define strategy to solve the problem
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.GLOBAL_CHEAPEST_ARC
+
+    solution = routing.SolveWithParameters(search_parameters)
+
+    if solution:
+        print_solution(manager, routing, solution, strategy="GLOBAL_CHEAPEST_ARC")
+
+    ''' Solving with Metaheuristic - GUIDED_LOCAL_SEARCH '''
+    # Resetting Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc after reset.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Define strategy to solve the problem
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    search_parameters.time_limit.seconds = time_limit
+    search_parameters.log_search = log_search
+
+    solution = routing.SolveWithParameters(search_parameters)
+
+    if solution:
+        print_solution(manager, routing, solution, strategy="Metaheuristic - GUIDED_LOCAL_SEARCH", time_limit=time_limit)
+
+    ''' Printing Optimum solution from TSPLIB '''
+    local_opt_solution_file_path = r"{}\tsp\files\TSP_a280_opt_tour.txt".format(dirname(getcwd()))
+    print_opt_solution(opt_file_path=local_opt_solution_file_path,
+                       distance_matrix=data['distance_matrix'])
 
 
 if __name__ == '__main__':
-    main()
-
+    main(time_limit=5, log_search=False)
---
+++ com/utils/intMatrix.py
@@ -0,0 +1,63 @@
+from com.utils.matrix import Matrix
+
+
+class IntMatrix(Matrix):
+    def construct_matrix_from_euclid_int_dist_2d_file(self, file_path):
+        points_matrix = self.make_euclid_2d_int_matrix_from_a_file(file_path=file_path)
+
+        for i in range(self.rows_size):
+            self.matrix[i][i] = 0
+
+            for j in range(i + 1, self.columns_size):
+                euclid_dist = self.calc_euclid_dist(x0=points_matrix[i][0],
+                                                    x1=points_matrix[j][0],
+                                                    y0=points_matrix[i][1],
+                                                    y1=points_matrix[j][1])
+
+                self.matrix[i][j] = euclid_dist
+                self.matrix[j][i] = euclid_dist
+
+    @classmethod
+    def make_euclid_2d_int_matrix_from_a_file(cls, file_path):
+        return_matrix = []
+        euclidian_points_file = open(file=file_path, mode='r')
+
+        for line in euclidian_points_file:
+            coordinates = cls.split_integer_coordinates_improved(line)
+            row = [coordinates[1], coordinates[2]]
+            return_matrix.append(row)
+
+        return return_matrix
+
+    @classmethod
+    def split_integer_coordinates_improved(cls, string):
+        splitted_coordinate = []
+        current_coordinate = None
+        incomplete_coordinate = False
+
+        string = string + ' '
+
+        for char in string:
+            if char != ' ':
+                if incomplete_coordinate:
+                    current_coordinate = current_coordinate + char
+                else:
+                    current_coordinate = char
+                    incomplete_coordinate = True
+            else:
+                if incomplete_coordinate:
+                    splitted_coordinate.append(int(current_coordinate))
+                incomplete_coordinate = False
+                current_coordinate = None
+
+        return splitted_coordinate
+
+
+if __name__ == '__main__':
+    from os import getcwd
+    from os.path import dirname
+    d2_matrix = IntMatrix(rows_size=280, columns_size=280)
+    d2_matrix.construct_matrix_from_euclid_int_dist_2d_file(file_path=r"{}\tsp\files\TSP_a280.txt".format(dirname(getcwd())))
+    print(len(d2_matrix.matrix))
+    d2_matrix.matrix_printer()
+
